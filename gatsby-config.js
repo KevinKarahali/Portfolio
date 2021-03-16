241f55511bd5842cc1505a02e67be48a14ce57bb
KevinKarahali <kevin.karahali@hotmail.com>
2021-03-16 12:02:29 +0100

    version 1.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `KevinKarahali`,
+    description: `Kevin Karahali porfolio, Backend developer.`,
+    author: `@kevinkarahali`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -25,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon-dev.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,54 @@
-import * as React from "react"
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 import SEO from "../components/seo"
-
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <div className="hero">
+      <div className="hero__content container">
+        <h2 className="no-margin hero__h2">
+          Hi Im web <a>developer</a>
+        </h2>
+        <div className="hero__img">
+          <StaticImage
+            className="hero-img"
+            src="../images/perfil2.png"
+            quality={100}
+            formats={["AUTO", "WEBP", "AVIF"]}
+            alt="Foto Perfil"
+          />
+        </div>
+      </div>
+    </div>
+    <h3 className=" container about-me">About me</h3>
+
+    <p className=" container p-info">
+      Coming from Caracas, Venezuela, I love programming . I have a special
+      interest in backend development even though I can also adapt to the
+      front-end, where I have done most of my projects and enhanced my skills. A
+      fan of learning and interacting with new technologies.
     </p>
-  </Layout>
+
+    <div className="git container">
+      <a
+        className="icon_github"
+        href="https://github.com/KevinKarahali"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <StaticImage
+          className="icon_github_static"
+          src="../images/github-icon.svg"
+          quality={100}
+          alt="icon-github"
+        />
+      </a>
+    </div>
+  </>
 )
 
 export default IndexPage
